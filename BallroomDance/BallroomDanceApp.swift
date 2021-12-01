b8274023fcf3ce4a8420b0c2b72b47023516dde9
Drew Brokamp <drew.brokamp@gmail.comm>
2021-11-30 21:24:10 -0500

    Initial Commit

---
+++ BallroomDance/BallroomDanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallroomDanceApp.swift
+//  BallroomDance
+//
+//  Created by Drew Brokamp on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct BallroomDanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
